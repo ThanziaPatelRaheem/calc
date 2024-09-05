a720bc70d6cae925a7ecd86250643145a17396f0
ThanziaPatelRaheem <thanzia.pr@gmail.com>
2024-09-05 16:14:41 +0800

    adding vendor file

---
+++ assets/scripts/vendor.js
@@ -0,0 +1,13 @@
+const userInput = document.getElementById("user-input");
+const addBtn = document.getElementById("add-btn");
+const subBtn = document.getElementById("sub-btn");
+const mulBtn = document.getElementById("mul-btn");
+const divBtn = document.getElementById("div-btn");
+const resetBtn = document.getElementById("reset-btn");
+const calcDescription = document.getElementById("calc-description");
+const calcOutput = document.getElementById("cal-output");
+
+function output(text, result) {
+  calcDescription.textContent = text;
+  calcOutput.textContent = result;
+}
